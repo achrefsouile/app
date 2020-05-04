d00725dac1d22dbe7d0549a0893e7b9dfafa3c3b
achrefsouile <achrefsouilem@gmail.com>
2020-05-04 18:59:31 -0400

    script3

--- lamp-install.sh
+++ lamp-install.sh
@@ -4,7 +4,7 @@
 apt update
 
 # Install Apache2, MySQL, PHP
-apt install apache2 mysql-server php php-mysql libapache2-mod-php php-cli
+apt install apache2 mysql-server php php-mysql libapache2-mod-php php-cli -y
 
 # Allow to run Apache on boot up
 systemctl enable apache2
@@ -25,5 +25,5 @@ rm /var/www/html/index.html
 cp index.html insert.php /var/www/html/
 
 #mysql
-mysql -e "create database test; CREATE USER 'user'@'localhost' IDENTIFIED BY 'abc'; GRANT ALL PRIVILEGES ON *.* TO 'user'@'localhost' IDENTIFIED BY 'abc'; use test; CREATE TABLE persons (first_name varchar(20), last_name varchar(20), email varchar(20) ); "
+mysql -e "create database demo; CREATE USER 'user'@'localhost' IDENTIFIED BY 'abc'; GRANT ALL PRIVILEGES ON demo.* TO 'user'@'localhost' IDENTIFIED BY 'abc'; use demo; CREATE TABLE persons (first_name varchar(20), last_name varchar(20), email varchar(20) ); "
 
